e723ab9b79a807fc96b672954e7edfe58d9f22fb
Jatin Dhankhar <dhankhar.jatin@gmail.com>
2018-07-19 12:19:18 +0530

    Add intial styling for the Main page

--- js/main.js
+++ js/main.js
@@ -157,29 +157,34 @@ fillRestaurantsHTML = (restaurants = self.restaurants) => {
  */
 createRestaurantHTML = (restaurant) => {
   const li = document.createElement('li');
-
+  // Add card class
+  li.classList.add("restaurant-card");
   const image = document.createElement('img');
   image.className = 'restaurant-img';
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
   li.append(image);
-
+  
+  const info = document.createElement('div');
+  info.classList.add('info');
+  
   const name = document.createElement('h1');
   name.innerHTML = restaurant.name;
-  li.append(name);
+  info.append(name);
 
   const neighborhood = document.createElement('p');
   neighborhood.innerHTML = restaurant.neighborhood;
-  li.append(neighborhood);
+  info.append(neighborhood);
 
   const address = document.createElement('p');
   address.innerHTML = restaurant.address;
-  li.append(address);
+  info.append(address);
 
   const more = document.createElement('a');
   more.innerHTML = 'View Details';
   more.href = DBHelper.urlForRestaurant(restaurant);
-  li.append(more)
+  info.append(more);
 
+  li.append(info);
   return li
 }
 
